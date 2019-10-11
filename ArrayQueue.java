a082e42b0b350f9806d5b6a42eb27edd020139b6
smmadden <smmadden10@gmail.com>
2019-10-10 22:28:52 -0700

    completed, tested, & commented

--- ArrayQueue.java
+++ ArrayQueue.java
@@ -4,12 +4,18 @@ public class ArrayQueue<T> implements Queue<T>
     private int front = 0;
     private int back = 0;
 
+    // takes an item out of the queue from the front (first in first out algorithm)
     public T dequeue(){
         T item = a[front];
         front = (front+1) % a.length;
         return item;
     }
 
+    // adds an item to the queue at the back
+    /* if the array is already full, that is, if the
+       new position goes one position past the front
+       wherever that may be in the array, then grow the array
+    */
     public void enqueue(T item){
         if((back+1) % a.length == front){
             grow_array();
@@ -18,10 +24,23 @@ public class ArrayQueue<T> implements Queue<T>
         back = back % a.length;
     }
 
+    // the queue is empty if the front is the same index as the back
     public boolean empty(){
         return (front == back);
     }
 
+    // grows the array to twice the size of the previous
+    /*
+       if the index of the front is less than the index of the back,
+       or in other words, if the font and back are in logical order,
+       then copy the items from front to back
+    */
+    /*
+       if the front is more than the back, if the queue circled around
+       in the array, then the values of the array need to be copied in two separate parts,
+       from front to the end of the array and then from the start of the array to the back
+     */
+    // the front and back are then reset so that they have the same values as before
     private void grow_array(){
         int newSize = a.length*2;
         T[] new_a = (T[]) new Object[newSize];
--- ArrayStack.java
+++ ArrayStack.java
@@ -1,8 +1,10 @@
 public class ArrayStack<T> implements Stack<T>
 {
     private T[] a = (T[]) new Object[10];
-    private int top = -1;
+    private int top = -1;                   // keeps track of the item last added to the stack
 
+    // adds an item to the stack
+    // if the stack is full, then we need to grow the array to accommodate another item
     public void push(T item){
         if(a.length-1 == top){
             grow_array();
@@ -10,18 +12,23 @@ public class ArrayStack<T> implements Stack<T>
         a[++top] = item;
     }
 
+    // returns the item on the top of the stack (first in last out algorithm)
     public T pop(){
         return a[top--];
     }
 
+    // program can look at what the item at the top of the stack is
     public T peek(){
         return a[top];
     }
 
+    // if the index for the top of the array is -1, that means that the stack is completely empty
     public boolean empty(){
         return (top==-1);
     }
 
+    // doubles the size of the array by creating a new array
+    // and copying every item into the new array
     private void grow_array(){
         int newSize = a.length*2;
         T[] new_a = (T[]) new Object[newSize];
